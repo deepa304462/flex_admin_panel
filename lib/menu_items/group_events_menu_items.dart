e8ae4ae9cf7acd3d60239aad9a994921309083d8
Deepa Pandey <dp304462@gmail.com>
2024-05-01 21:36:00 +0530

    group  event  part done pending status , created by and payouts

--- lib/menu_items/group_events_menu_items.dart
+++ lib/menu_items/group_events_menu_items.dart
@@ -1,9 +1,8 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../constants/colors.dart';
 import '../constants/styles.dart';
-
 
 class GroupMenuItems extends StatefulWidget {
   const GroupMenuItems({super.key});
@@ -13,193 +12,187 @@ class GroupMenuItems extends StatefulWidget {
 }
 
 class _GroupMenuItemsState extends State<GroupMenuItems> {
-  final List<Map<String, String>> items = [
-    {
-      'number': '20',
-      'title': 'Chelea will beat monutd',
-      'category': 'Sports',
-      'status': 'Live',
-      'user': 'ADMIN',
-      'option1': 'YES',
-      'option2': 'NO',
-      'action': 'RELEASE PAYMENT',
-    },
-  ];
+  Map<String, bool> loadingStates = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:   Padding(
+      body: Padding(
         padding: const EdgeInsets.all(24.0),
         child: Container(
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
-            border:
-            Border.all(color: Colors.grey.shade200, width: 1),
+            border: Border.all(color: Colors.grey.shade200, width: 1),
             borderRadius: BorderRadius.circular(4),
           ),
           child: Column(
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 48,
-                decoration: BoxDecoration(
-                    color: ColorConstant.gray2,
-                    border: Border.all(color: Colors.grey.shade400, width: 1)
-                ),
-                child: Padding(
-                  padding:
-                  const EdgeInsets.only(left: 8,right: 8),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Players",
-                        style: Styles.textWith9with600(
-                            ColorConstant.gray500),
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        "Events name",
-                        style: Styles.textWith14with600(
-                            ColorConstant.gray500),
-                      ),
-                      const SizedBox(
-                        width: 200,
-                      ),
-                      Text(
-                        "Category",
-                        style: Styles.textWith14with600(
-                            ColorConstant.gray500),
-                      ),
-                      const SizedBox(
-                        width: 60,
-                      ),
-                      Text(
-                        "Status",
-                        style: Styles.textWith14with600(
-                            ColorConstant.gray500),
-                      ),
-                      const SizedBox(
-                        width: 60,
-                      ),
-                      Text(
-                        "Group Owner",
-                        style: Styles.textWith14with600(
-                            ColorConstant.gray500),
-                      ),
-                      const SizedBox(
-                        width: 60,
-                      ),
-                      Text(
-                        "CHOOSE OUTCOME",
-                        style: Styles.textWith14with600(
-                            ColorConstant.gray500),
-                      ),
-                      const SizedBox(
-                        width: 60,
-                      ),
-                      Text(
-                        "PAYOUT TO WINNERS",
-                        style: Styles.textWith14with600(
-                            ColorConstant.gray500),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.only(left: 8.0),
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: 10,
-                    itemBuilder: (context,index){
-                      return Row(
-                        children: [
-                          Container(
-                            height: 80,
-                            width: 50,
-                            color: ColorConstant.gray2,
-                            child: Center(child: Text("20",style: Styles.textWith10with600(ColorConstant.gray500),)),
-                          ),
-                          const SizedBox(
-                            width: 4,
-                          ),
-                          Center(child: Text("Chelea will beat monutd",style: Styles.textWith16with600(ColorConstant.gray700),)),
-                          const SizedBox(
-                            width: 90,
-                          ),
-                          Center(child: Text("Sports",style: Styles.textWith16with600(ColorConstant.gray700),)),
-                          const SizedBox(
-                            width: 70,
-                          ),
-                          Container(
-                              decoration: BoxDecoration(
-                                  color: ColorConstant.liveColor,
-                                  borderRadius: BorderRadius.circular(4)
-                              ),
-                              child: Center(child: Padding(
-                                padding: const EdgeInsets.only(left: 8.0,right: 8,top: 8,bottom: 8),
-                                child: Text("Live",style: Styles.textWith14with600(Colors.white)),
-                              ))),
-                          const SizedBox(
-                            width: 70,
-                          ),
-                          Center(child: Text("ADMIN",style: Styles.textWith14with600(ColorConstant.Blue400))),
-                          const SizedBox(
-                            width: 80,
-                          ),
-                          Row(
-                            children: [
-                              Container(
-                                  decoration: BoxDecoration(
-                                      color: ColorConstant.liveColor,
-                                      borderRadius: BorderRadius.circular(4)
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance.collection('chatrooms').snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: DataTable(
+                          columns: [
+                            DataColumn(
+                                label: Text(
+                                  "Players",
+                                  style: Styles.textWith9with600(ColorConstant.gray500),
+                                )),
+                            DataColumn(
+                                label: Text("Events name", style: Styles.textWith14with600(ColorConstant.gray500))),
+                            DataColumn(
+                                label: Text("Category", style: Styles.textWith14with600(ColorConstant.gray500))),
+                            DataColumn(
+                                label: Text("Status", style: Styles.textWith14with600(ColorConstant.gray500))),
+                            DataColumn(
+                                label: Text("Created by", style: Styles.textWith14with600(ColorConstant.gray500))),
+                            DataColumn(
+                                label: Text("CHOOSE OUTCOME", style: Styles.textWith14with600(ColorConstant.gray500))),
+                            DataColumn(
+                                label: Text("PAYOUT TO WINNERS", style: Styles.textWith14with600(ColorConstant.gray500))),
+                          ],
+                          rows: snapshot.data!.docs.map<DataRow>((doc) {
+                            var memberList = doc['members'];
+                            var member = memberList != null ? memberList.length.toString() : 'N/A'; // Default value if 'members' field doesn't exist
+                            return DataRow(
+                              cells: [
+                                DataCell(
+                                  Text(
+                                    member,
+                                    style: Styles.textWith14with600(ColorConstant.gray500),
                                   ),
-                                  child: Center(child: Padding(
-                                    padding: const EdgeInsets.only(left: 8.0,right: 8,top: 8,bottom: 8),
-                                    child: Text("YES",style: Styles.textWith14with900(Colors.white)),
-                                  ))),
-                              const SizedBox(
-                                width: 16,
-                              ),
-                              Container(
-                                  decoration: BoxDecoration(
-                                      color:ColorConstant.gray3,
-                                      borderRadius: BorderRadius.circular(4)
+                                ),
+                                DataCell(SizedBox(
+                                  width: 400,
+                                  child: Text(
+                                    doc['groupName'],
+                                    style: Styles.textWith16with600(ColorConstant.gray700),
+                                    maxLines: 2,
                                   ),
-                                  child: Center(child: Padding(
-                                    padding: const EdgeInsets.only(left: 8.0,right: 8,top: 8,bottom: 8),
-                                    child: Text("NO",style: Styles.textWith14with900(Colors.white)),
-                                  ))),
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 100,
-                          ),
-                          Container(
-                              decoration: BoxDecoration(
-                                  color:ColorConstant.releaseButtonColor,
-                                  borderRadius: BorderRadius.circular(20)
-                              ),
-                              child: Center(child: Padding(
-                                padding: const EdgeInsets.only(left: 12.0,right: 12,top: 8,bottom: 8),
-                                child: Text("RELEASE PAYMENT",style: Styles.textWith14with600(Colors.white)),
-                              ))),
-                
-                        ],
+                                )),
+                                DataCell(Text(
+                                  doc['category'],
+                                  style: Styles.textWith14with600(ColorConstant.gray700),
+                                )),
+                                DataCell(Container(
+                                    decoration: BoxDecoration(
+                                        color:  ColorConstant.liveColor,
+                                        borderRadius: BorderRadius.circular(4)),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text("Live",
+                                          style: Styles.textWith14with600(Colors.white)),
+                                    ))),
+                                DataCell(Text("LOL",
+                                    style: Styles.textWith14with600(ColorConstant.Blue400))),
+                                DataCell(Row(
+                                  children: [
+                                    InkWell(
+                                      onTap: () async {
+                                        setState(() {
+                                          loadingStates[doc['groupId']] = true;
+                                        });
+                                        addDataToExistingDocument(doc['groupId'], "Yes");
+
+                                      },
+                                      child: loadingStates[doc['groupId']] ?? false
+                                          ? const Center(child: CircularProgressIndicator())
+                                          : Container(
+                                        decoration: BoxDecoration(
+                                            color: getColorYes(doc),
+                                            borderRadius: BorderRadius.circular(4)),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text("Yes",
+                                              style: Styles.textWith14with900(Colors.white)),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 16),
+                                    InkWell(
+                                      onTap: () async {
+                                        setState(() {
+                                          loadingStates[doc['groupId']] = true;
+                                        });
+                                        addDataToExistingDocument(doc['groupId'], "No");
+
+                                      },
+                                      child: loadingStates[doc['groupId']] ?? false
+                                          ? const Center(child: CircularProgressIndicator())
+                                          : Container(
+                                        decoration: BoxDecoration(
+                                            color: getColorNo(doc),
+                                            borderRadius: BorderRadius.circular(4)),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text("NO",
+                                              style: Styles.textWith14with900(Colors.white)),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                                DataCell(Container(
+                                    decoration: BoxDecoration(
+                                        color:  ColorConstant.releaseButtonColor,
+                                        borderRadius: BorderRadius.circular(20)),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text("RELEASE PAYMENT",
+                                          style: Styles.textWith14with600(Colors.white)),
+                                    ))),
+                              ],
+                            );
+                          }).toList(),
+                        ),
                       );
                     },
-                
                   ),
                 ),
               ),
-
             ],
           ),
         ),
       ),
     );
+  }
+
+  void addDataToExistingDocument(String doc, String outcome) async {
+    DocumentReference documentRef = FirebaseFirestore.instance.collection('chatrooms').doc(doc);
+
+    // Update the document by adding or updating fields
+    await documentRef.update({'outcome': outcome}).catchError((onError) {
+      print(onError);
+    });
+    setState(() {
+      loadingStates[doc] = false;
+    });
+  }
+
+  getColorYes(QueryDocumentSnapshot<Object?> doc) {
+
+    try{
+      return (doc['outcome'] == null ? ColorConstant.liveColor : (doc['outcome'] == "Yes" ? ColorConstant.gray : ColorConstant.liveColor));
+    }catch(e){
+      return ColorConstant.liveColor;
+    }
+  }
+
+  getColorNo(QueryDocumentSnapshot<Object?> doc) {
+
+    try{
+      return (doc['outcome'] == null ? ColorConstant.liveColor : (doc['outcome'] == "No" ? ColorConstant.gray :Colors.red));
+    }catch(e){
+      return Colors.red;
+    }
   }
 }
